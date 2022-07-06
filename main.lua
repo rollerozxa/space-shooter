3ccc586333fbb4562a018654887e49b306126c17
ROllerozxa <temporaryemail4meh+github@gmail.com>
2022-07-06 23:34:21 +0200

    Add shootey shootey bullets haha

--- main.lua
+++ main.lua
@@ -25,7 +25,8 @@ function love.load()
 	love.graphics.setDefaultFilter('nearest', 'nearest', 4)
 
 	assets = {
-		player = NewImage("ball")
+		player = NewImage("ball"),
+		bullet = NewImage("bullet")
 	}
 
 	fonts = {
@@ -36,6 +37,12 @@ function love.load()
 end
 
 local playing = true
+
+local bullets = {
+
+}
+
+local oldspacedown
 
 function love.update()
 	if love.keyboard.isDown("a") then
@@ -61,14 +68,39 @@ function love.update()
 
 	player.vel = math.lerp(player.vel, 0, 0.05)
 	player.tilt = math.lerp(player.tilt, 0, 0.1)
+
+	if love.keyboard.isDown("space") and not oldspacedown then
+		local newbullet = Obj(player.x, player.y, 16, 8)
+		newbullet.angle = player.angle
+
+		table.insert(bullets, newbullet)
+	end
+	--oldspacedown = love.keyboard.isDown("space")
+
+	for id, bullet in pairs(bullets) do
+		bullet.x = bullet.x + (10*math.cos(bullet.angle))
+		bullet.y = bullet.y + (10*math.sin(bullet.angle))
+
+		-- Drop bullet if too far off screen
+		if not CheckCollision(-50,-50,resolution.x+100,resolution.y+100, bullet.x, bullet.y, bullet.size.x, bullet.size.y) then
+			bullets[id] = nil
+		end
+	end
 end
 
 function love.draw()
 	love.graphics.setBackgroundColor(0.1,0.1,0.1)
 	love.graphics.setColor(0.9,0.9,0.9)
 
+	local numbullets = 0
+
+	for _,bullet in pairs(bullets) do
+		love.graphics.draw(assets.bullet, bullet.x, bullet.y, bullet.angle, 1, 1)
+		numbullets = numbullets + 1
+	end
+
 	love.graphics.draw(assets.player, player.x, player.y, player.angle, 4, 4, player.size.x/8, player.size.y/8)
 
 	love.graphics.setFont(fonts.default)
-	love.graphics.print("FPS: "..love.timer.getFPS(), 5, 10)
+	love.graphics.print("FPS: "..love.timer.getFPS()..", #bullets = "..numbullets, 5, 10)
 end
